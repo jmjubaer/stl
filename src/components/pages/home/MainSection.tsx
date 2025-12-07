7c997971d74a1e952de3d65c2fb40291fdaa775a
jmjubaer <jmjubaer3927@gmail.com>
2025-12-07 15:16:07 +0600

    create resuseable  tag filter components

--- src/components/pages/home/MainSection.tsx
+++ src/components/pages/home/MainSection.tsx
@@ -3,23 +3,41 @@ import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import { FaList } from "react-icons/fa6";
 import { IoGrid } from "react-icons/io5";
+import { LuArrowDownUp } from "react-icons/lu";
+import TagDropdown from "../../ui/TagDropdown";
 
 const MainSection = () => {
     const [layout, setLayout] = useState<"grid" | "list">("grid");
+    const [openTag, setOpenTag] = useState(false);
+    const [tag, setTag] = useState<string[]>([]);
 
     return (
         <section className=''>
-            <div className=' py-3 shadow-sm dark:shadow-md dark:shadow-secondary/20'>
+            <div className=' py-3 shadow-sm dark:shadow-md dark:shadow-text/10'>
                 <div className='flex items-center justify-between container'>
-                    <div className=''>
+                    <div className='flex items-center gap-4'>
                         {/* Search bar */}
                         <div className='relative'>
-                            <FaSearch className="absolute top-3 left-3 text-text/50"/>
+                            <FaSearch className='absolute top-3 left-3 text-text/50' />
                             <input
                                 type='search'
                                 placeholder='Search for products...'
-                                className='px-4 py-1.5 border border-text/20 rounded-2xl w-80 outline-none pl-9'
+                                className='px-4 py-1.5 border border-text/20 rounded-xl w-80 outline-none pl-9'
                             />
+                        </div>
+                        {/* Tag filter */}
+                        <TagDropdown
+                            openTag={openTag}
+                            setOpenTag={setOpenTag}
+                            tag={tag}
+                            setTag={setTag}
+                        />
+                        {/* Sort by */}
+                        <div className=''>
+                            <button className='px-5 py-1.5 border border-text/20 rounded-xl outline-none cursor-pointer hover:bg-primary hover:text-white flex items-center duration-500'>
+                                <LuArrowDownUp className='inline mr-2 text-xl' />{" "}
+                                <span className='inline'>Newest first</span>
+                            </button>
                         </div>
                     </div>
                     {/* Grid change section */}
---
+++ src/components/ui/TagDropdown.tsx
@@ -0,0 +1,84 @@
+import { useEffect, useRef } from "react";
+import { CiFilter } from "react-icons/ci";
+import { FaCheck } from "react-icons/fa6";
+type TagDropdownProps = {
+    openTag: boolean;
+    setOpenTag: React.Dispatch<React.SetStateAction<boolean>>;
+    tag: string[];
+    setTag: React.Dispatch<React.SetStateAction<string[]>>;
+};
+const TagDropdown = ({
+    openTag,
+    setOpenTag,
+    tag,
+    setTag,
+}: TagDropdownProps) => {
+    const handleToggleTag = (tag: string) => {
+        setTag((prevTag) =>
+            prevTag.includes(tag)
+                ? prevTag.filter((t) => t !== tag)
+                : [...prevTag, tag]
+        );
+    };
+    const tagRef = useRef<HTMLDivElement>(null);
+
+    // Close dropdown when clicking outside
+    useEffect(() => {
+        const handleClickOutside = (e: MouseEvent) => {
+            if (tagRef.current && !tagRef.current.contains(e.target as Node)) {
+                setOpenTag(false);
+            }
+        };
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () =>
+            document.removeEventListener("mousedown", handleClickOutside);
+    }, []);
+    const tagList = [
+        { name: "Design", color: "#9952E0" },
+        { name: "Development", color: "#1A8CFF" },
+        { name: "Tutorial", color: "#28BD66" },
+        { name: "Marketing", color: "#F97A1F" },
+        { name: "Inspiration", color: "#1DBAC9" },
+    ];
+    return (
+        <div className='relative' ref={tagRef}>
+            <button
+                onClick={() => setOpenTag(!openTag)}
+                className='px-5 py-1.5 border border-text/20 rounded-xl outline-none cursor-pointer hover:bg-primary hover:text-white flex items-center duration-500'>
+                <CiFilter className='inline mr-1 text-xl' />{" "}
+                <span className='inline'>Tags</span>
+            </button>
+            <ul
+                className={`absolute top-10 border border-text/20 p-1 px-1.5 rounded-xl left-0 bg-background shadow-md dark:shadow-white/20 w-48 ${
+                    openTag ? "visible" : "hidden"
+                }`}>
+                {tagList.map((tagItem) => (
+                    <li
+                        key={tagItem.name}
+                        onClick={() => handleToggleTag(tagItem.name)}
+                        className='px-3 py-2 rounded-xl text-xs hover:bg-text/5 cursor-pointer flex items-center gap-3'>
+                        <FaCheck
+                            className={`text-sm ${
+                                tag.includes(tagItem.name)
+                                    ? "visible"
+                                    : "invisible"
+                            }`}
+                        />
+
+                        <span
+                            style={{
+                                backgroundColor: tagItem.color + "20",
+                                color: tagItem.color,
+                            }}
+                            className='p-1  font-bold px-3  rounded-full'>
+                            {tagItem.name}
+                        </span>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default TagDropdown;
