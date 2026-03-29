313ca38f76baec41af1ba45630b6b2b44995c11e
jmjubaer <jmjubaer3927@gmail.com>
2026-03-29 23:22:32 +0600

    set up redux persist

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,40 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./features/authSlice";
-
+import storage from "redux-persist/lib/storage";
+import {
+    FLUSH,
+    PAUSE,
+    PERSIST,
+    persistReducer,
+    persistStore,
+    PURGE,
+    REGISTER,
+    REHYDRATE,
+} from "redux-persist";
+const persistConfig = {
+    key: "auth",
+    storage,
+};
+const persistedAuthReducer = persistReducer(persistConfig, authSlice);
 export const store = configureStore({
     reducer: {
-        auth: authSlice,
+        auth: persistedAuthReducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                // ignore for redux persist
+                ignoredActions: [
+                    FLUSH,
+                    REHYDRATE,
+                    PAUSE,
+                    PERSIST,
+                    PURGE,
+                    REGISTER,
+                ],
+            },
+        }),
 });
-
+export const persistedStore = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
